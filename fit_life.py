d62fcfe712e99f98d9cc6e22694da1b27cb90dbc
Maxim Smorygo <mak-sim1999@mail.ru>
2026-09-05 22:45:34 +0300

    Не пройден один из тестов, где указано проверить порядок ввода данных, хотя порядок верный

--- fit_life.py
+++ fit_life.py
@@ -1,26 +1,88 @@
-# Проект FitLife - MVP версия 1.0
+WATER_PER_KG = 30
+ML_IN_L = 1000
 
 
-# 1. Знакомство
-# TODO: Спроси у пользователя имя и сохрани в переменную user_name
-# TODO: Спроси возраст и сохрани в переменную user_age (не забудь преобразовать в число)
+def _calc_bmi(user_weight, user_height):
+    bmi = user_weight / (user_height ** 2)
+    return round(bmi, 1)
 
 
-# 2. Сбор данных
-# TODO: Запроси вес (в кг) и сохрани в user_weight (тип float)
-# TODO: Запроси рост (в метрах, например 1.75) и сохрани в user_height (тип float)
+def _calc_water_volume(user_weight):
+    water_ml = user_weight * WATER_PER_KG
+    water_l = water_ml / ML_IN_L
+    return round(water_l, 2)
 
 
-# 3. Логика расчетов (Функции как "черный ящик": используем арифметику)
-# Формула ИМТ: вес разделить на (рост в квадрате)
-# TODO: Рассчитай bmi (Индекс массы тела)
+def _get_user_age():
+    user_age = int(input('Теперь подскажите свой возраст (полных лет):\n'))
+    if user_age >= 50:
+        print("\nВ таком возрасте важно следить за объемом потребляемой воды!")
+    else:
+        print(
+            "\nЕще вся жизнь впереди, но все равно сейчас расчитаем сколько "
+            "нужно воды, чтобы прожить долгую и счастливую жизнь.\n"
+        )
+    return user_age
 
 
-# Подсчет воды: вес * 30 мл
-# TODO: Рассчитай water_needed
+def _get_user_weight():
+    user_weight = float(input(
+        f"{user_name}, теперь укажите свой вес в кг.\nЗнаю, вопрос не "
+        "скромный, но без этого я не смогу грамотно произвести вычисления!\n"
+        "(Для разделения десятичных дробей используйте точку)\n"
+    ))
+    return user_weight
 
 
-# 4. Вывод красивого результата
-# TODO: Используй f-строку, чтобы вывести приветствие, например: "Привет, Иван!"
-# TODO: Выведи возраст, ИМТ (округленный до 1 знака) и норму воды.
-print("Расчет окончен. Будьте здоровы!")
+def _get_user_height():
+    user_height = float(input(
+        "\nИ последнее, что я уточню - это Ваш рост.\n"
+        "Значение укажите в метрах.\n"
+        "(Для разделения десятичных дробей используйте точку)\n"
+    ))
+    return user_height
+
+
+def _get_last_check():
+    print(
+        f"{user_name}, перед началом расчетов давайте подтвердим данные."
+        f"\nВаш возраст - {user_age} лет"
+        f"\nВаш вес = {user_weight} кг."
+        f"\nВаш рост = {user_height} м."
+        "\nЕсли данные верны, то введите 'Да', если нет введите 'Нет' и у вас "
+        "будет возможность ввести показатели еще раз.",
+        sep=""
+    )
+    return input()
+
+
+with open("intro.txt", "r", encoding="utf-8") as file_intro:
+    for line in file_intro:
+        print(line.strip())
+
+user_name = input()
+print("\nРад знакомству, ", user_name, "!", sep="", flush=True)
+
+user_age = _get_user_age()
+user_weight = _get_user_weight()
+user_height = _get_user_height()
+last_check = _get_last_check()
+
+while True:
+    if last_check.lower() == "да":
+        bmi = _calc_bmi(user_weight, user_height)
+        water_volume = _calc_water_volume(user_weight)
+        break
+    else:
+        user_age = _get_user_age()
+        user_weight = _get_user_weight()
+        user_height = _get_user_height()
+        last_check = _get_last_check()
+
+print(
+    f"\nОтчет для пользователя: {user_name} ({user_age})"
+    f"\nВаш индекс массы тела = {bmi}"
+    f"\nРекомендуемая норма воды: {water_volume} л. воды в день"
+    "\n\nСпасибо большое, что дали возможность проявить себя! Будьте здоровы!",
+    sep=""
+)
