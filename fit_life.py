b79755c8bf4bb45a38dfd3f61c3f93fcdcc6c73f
Maxim Smorygo <mak-sim1999@mail.ru>
2026-09-06 18:21:45 +0300

    По комментариям Романа упростил формулу расчета объема воды, поставил циклы с проверкой на введеные значения и вернул цикл с проверкой введенных данных из первой версии, которую убрал, потому что тест ругался на порядок вводимых данных, тк проверка - дополнительный ввод со стороны пользователя

--- fit_life.py
+++ fit_life.py
@@ -8,13 +8,19 @@ def _calc_bmi(user_weight, user_height):
 
 
 def _calc_water_volume(user_weight):
-    water_ml = user_weight * WATER_PER_KG
-    water_l = water_ml / ML_IN_L
+    water_l = (user_weight * WATER_PER_KG) / ML_IN_L
     return round(water_l, 2)
 
 
 def _get_user_age():
-    user_age = int(input('Теперь подскажите свой возраст (полных лет):\n'))
+    while True:
+        try:
+            user_age = int(input(
+                "Теперь подскажите свой возраст (полных лет):\n"
+            ))
+            break
+        except ValueError:
+            print("Значение нужно ввести в числовом формате, например, 33")
     if user_age >= 50:
         print("\nВ таком возрасте важно следить за объемом потребляемой воды!")
     else:
@@ -26,19 +32,46 @@ def _get_user_age():
 
 
 def _get_user_weight():
-    return float(input(
+    print(
         f"{user_name}, теперь укажите свой вес в кг.\nЗнаю, вопрос не "
         "скромный, но без этого я не смогу грамотно произвести вычисления!\n"
         "(Для разделения десятичных дробей используйте точку)\n",
-    ))
+    )
+    while True:
+        try:
+            user_weight = float(input())
+            break
+        except ValueError:
+            print("Значение нужно ввести в числовом формате, например, 33")
+    return user_weight
 
 
 def _get_user_height():
-    return float(input(
+    print(
         "\nИ последнее, что я уточню - это Ваш рост.\n"
         "Значение укажите в метрах.\n"
         "(Для разделения десятичных дробей используйте точку)\n",
-    ))
+    )
+    while True:
+        try:
+            user_height = float(input())
+            break
+        except ValueError:
+            print("Значение нужно ввести в числовом формате, например, 33")
+    return user_height
+
+
+def _get_last_check():
+    print(
+        f"{user_name}, перед началом расчетов давайте подтвердим данные."
+        f"\nВаш возраст - {user_age} лет"
+        f"\nВаш вес = {user_weight} кг."
+        f"\nВаш рост = {user_height} м."
+        "\nЕсли данные верны, то введите 'Да', если нет введите 'Нет' и у вас "
+        "будет возможность ввести показатели еще раз.",
+        sep=""
+    )
+    return input()
 
 
 print(
@@ -55,9 +88,18 @@ print("\nРад знакомству, ", user_name, "!", sep="", flush=True)
 user_age = _get_user_age()
 user_weight = _get_user_weight()
 user_height = _get_user_height()
+last_check = _get_last_check()
 
-bmi = _calc_bmi(user_weight, user_height)
-water_volume = _calc_water_volume(user_weight)
+while True:
+    if last_check.lower() == "да":
+        bmi = _calc_bmi(user_weight, user_height)
+        water_volume = _calc_water_volume(user_weight)
+        break
+    else:
+        user_age = _get_user_age()
+        user_weight = _get_user_weight()
+        user_height = _get_user_height()
+        last_check = _get_last_check()
 
 print(
     f"\nОтчет для пользователя: {user_name} ({user_age})"
